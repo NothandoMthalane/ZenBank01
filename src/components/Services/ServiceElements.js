83eb63d5839375758605435d74329297712ee70b
Nothando <nothandomthalane@gmail.com>
2022-04-18 13:39:20 +0200

    Update ServiceElements.js

--- src/components/Services/ServiceElements.js
+++ src/components/Services/ServiceElements.js
@@ -15,7 +15,7 @@ export const ServicesContainer = styled.div`
   @media screen and (max-width: 768px){
     height: 1300px;
   }
-`;
+`
 
 export const ServicesWrapper = styled.div`
   max-width: 1000px;
@@ -34,7 +34,7 @@ export const ServicesWrapper = styled.div`
     grid-template-columns: 1fr;
     padding: 0 1.4rem;
 }
-`;
+`
 
 export const ServicesCard = styled.div`
   background: #fff;
@@ -53,13 +53,13 @@ export const ServicesCard = styled.div`
       transition: all 0.2s ease-in-out;
       cursor: pointer;
   }
-`;
+`
 
 export const ServicesIcon = styled.img`
   height: 160px;
   width: 160px;
   margin-bottom: .5rem;
-`;
+`
 
 export const ServicesH1 = styled.h1`
   font-size: 2.5rem;
@@ -69,14 +69,14 @@ export const ServicesH1 = styled.h1`
   @media screen and (max-width: 480px){
       font-size: 2rem;
   }
-`;
+`
 
 export const ServicesH2 = styled.h2`
   font-size: 1.4rem;
   margin-bottomL .5rem;
-`;
+`
 
 export const ServicesP = styled.p`
   font-size: 1rem;
   text-align: center;
-`;
+`
